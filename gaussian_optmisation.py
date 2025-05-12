f841a4a26d13d6fcbdf9db52c88969bc866125cf
Benjaminwills <benjaminwills047@gmail.com>
2025-05-12 22:16:39 +0100

    Add epsilon coverage and docstrings

--- gaussian_optmisation.py
+++ gaussian_optmisation.py
@@ -19,7 +19,35 @@ class One_d_gaussian_process_optimiser:
         upper_bound: float,
         kernel: callable,
         aqcuisition_function: callable,
+        epsilon: float = 0.3,
     ):
+        """This class is a one dimensional gaussian proess optimiser. It is used to
+        optimise a function in one dimension. It uses a gaussian process to model the
+        function, and an acquisition function to find the next point to sample and then
+        to update the gaussian process.
+
+        Parameters
+        ----------
+        evaluation_function : callable
+            The function that we want to optimise. It should take a single input and
+            return a single output. Note that the functions usually optimised in this
+            way are expensive to evaluate, so we want to sample as few points as possible.
+        lower_bound : float
+            The lower bound of the search space.
+        upper_bound : float
+            The upper bound of the search space.
+        kernel : callable
+            The kernel function to use for the gaussian process. It should take two inputs
+            and return a single output.
+        aqcuisition_function : callable
+            The acquisition function to use for the gaussian process. It should take
+            three inputs: the input point, the gaussian process and the maximum value of
+            the function at the given timestamp. It should return a single output.
+        epsilon : float, optional
+            The exploration / exploitation trade off. High epsilon means
+            high amounts of exploitation in regions with high
+            standard deviation, by default 0.3
+        """
         # Define the evaluation function to sample points from
         self.evaluation_function = evaluation_function
 
@@ -33,6 +61,9 @@ class One_d_gaussian_process_optimiser:
         # Define the acquisition function
         self.acquisition_function = aqcuisition_function
 
+        # Define the exploration / exploitation trade off
+        self.epsilon = epsilon
+
     def choose_random_point(self):
         return np.random.uniform(self.lower_bound, self.upper_bound)
 
@@ -41,7 +72,6 @@ class One_d_gaussian_process_optimiser:
         fitted_gaussian_process: One_d_gaussian_process,
         acquisition_function: callable,
         maximum_value: float,
-        epsilon: float = 0.3,
     ) -> float:
 
         # Get the next point using the probability of improvement, we want to omptimise
@@ -50,7 +80,7 @@ class One_d_gaussian_process_optimiser:
             x,
             fitted_gaussian_process,
             maximum_value,
-            epsilon=epsilon,
+            epsilon=self.epsilon,
         )
         next_point = minimize_scalar(
             objective_function,
@@ -103,7 +133,6 @@ class One_d_gaussian_process_optimiser:
                 fitted_gaussian_process=fitted_gaussian_process,
                 acquisition_function=self.acquisition_function,
                 maximum_value=maximum_func_value,
-                epsilon=0.3,
             )
 
             # Update the training arrays
@@ -120,7 +149,7 @@ class One_d_gaussian_process_optimiser:
                 stagnation_counter += 1
 
             # If the maximum value has not changed in the last 10 iterations, break
-            if stagnation_counter > 10:
+            if stagnation_counter > max_iterations / 2:
                 print(
                     f"Maximum value has not changed in the last 10 iterations, stopping optimisation."
                 )
@@ -139,18 +168,19 @@ class One_d_gaussian_process_optimiser:
 if __name__ == "__main__":
     # Define a function to be optimised
     def func(x):
-        return -5 * x**2 + 10
+        return x * np.cos(x)
 
     optimiser = One_d_gaussian_process_optimiser(
         evaluation_function=func,
-        lower_bound=-5,
-        upper_bound=5,
+        lower_bound=-10,
+        upper_bound=10,
         kernel=rbf_kernel,
-        aqcuisition_function=expected_improvement,
+        aqcuisition_function=probability_of_improvement,
+        epsilon=1,
     )
 
     fitted_gaussian_process, training_data, training_outputs = optimiser.optimise(
-        max_iterations=20
+        max_iterations=100
     )
 
     # Plot the results
@@ -165,7 +195,7 @@ if __name__ == "__main__":
     plt.plot(
         fitted_gaussian_process.testing_range,
         [
-            expected_improvement(x, fitted_gaussian_process, max_y)
+            probability_of_improvement(x, fitted_gaussian_process, max_y)
             for x in fitted_gaussian_process.testing_range
         ],
         label="Acquisition function",
